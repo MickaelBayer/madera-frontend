3514b76c8dbc9b8e6260f51501a3a071c08f6fd9
audrey <nunes.audrey31@gmail.com>
2019-12-05 17:26:35 +0100

    [ADD] ajout vuetify

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue';
+import Vuetify from 'vuetify';
 import App from './App.vue';
 import router from './router';
 import './registerServiceWorker';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 Vue.config.productionTip = false;
-
+Vue.use(Vuetify);
+export default new Vuetify({ });
 new Vue({
   router,
   render: h => h(App),
+  vuetify: new Vuetify(),
 }).$mount('#app');
