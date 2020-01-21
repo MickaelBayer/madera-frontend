4e2ddc78c61f02107890cde1bac907324905bcb1
Pauline SOCA <pso@VAL.local>
2020-01-21 13:47:55 +0100

    transmition three.js

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     #nav
       router-link(to="/") Home
       router-link(to="/about") | About
+      router-link(to="/modelisation") | Modelisation
     router-view
 </template>
 
---
+++ src/views/Modelisation.vue
@@ -0,0 +1,56 @@
+<template>
+    <div id="container"></div>
+</template>
+
+<script>
+import * as Three from 'three'
+
+export default {
+  name: 'ThreeTest',
+  data() {
+    return {
+      camera: null,
+      scene: null,
+      renderer: null,
+      mesh: null
+    }
+  },
+  methods: {
+    init: function() {
+        alert('init');
+        let container = document.getElementById('container');
+
+        this.camera = new Three.PerspectiveCamera(70, container.clientWidth/container.clientHeight, 0.01, 10);
+        this.camera.position.z = 1;
+
+        this.scene = new Three.Scene();
+
+        let geometry = new Three.BoxGeometry(0.2, 0.2, 0.2);
+        let material = new Three.MeshNormalMaterial();
+
+        this.mesh = new Three.Mesh(geometry, material);
+        this.scene.add(this.mesh);
+
+        this.renderer = new Three.WebGLRenderer({antialias: true});
+        this.renderer.setSize(container.clientWidth, container.clientHeight);
+        container.appendChild(this.renderer.domElement);
+
+    },
+    animate: function() {
+      alert('animate');
+       
+        this.mesh.rotation.x += 0.01;
+        this.mesh.rotation.y += 0.02;
+        this.renderer.render(this.scene, this.camera);
+    }
+  },
+  mounted() {
+    alert('begin');
+      this.init();
+      this.animate();
+  }
+}
+</script>
+
+<style scoped>
+</style>
