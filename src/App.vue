0018b0431a79919da847d6dc2a1e4b466f009784
audrey <nunes.audrey31@gmail.com>
2020-02-12 18:21:01 +0100

    [ADD] header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template lang="pug">
   v-app
+    Header
     router-view
 </template>
 
 <script>
+  import Header from './components/Header';
 export default {
   name: 'App',
+  components: {Header},
   data() {
     return {
     }
@@ -16,17 +19,17 @@ export default {
 </script>
 
 <style lang="sass">
-#app
-  font-family: 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  text-align: center
-  color: #2c3e50
+  #app
+    font-family: 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing: antialiased
+    -moz-osx-font-smoothing: grayscale
+    text-align: center
+    color: #2c3e50
 
-#nav
-  padding: 30px
+  #nav
+    padding: 30px
 
 
-#nav a.router-link-exact-active
-  color: #42b983
+  #nav a.router-link-exact-active
+    color: #42b983
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+  .topBar
+    .logoMadera
+      img(src="../assets/logo.png")
+    .btnMyAccount
+      v-btn(large=true color="#409a1b" style="color: white" ) Mon compte
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+    data() {
+      return {
+      }
+    },
+    mounted() {
+      // todo ne pas afficher le bouton mon compte si l'utilisateur n'est pas connecté
+    }
+  }
+</script>
+
+<style lang="sass">
+  .topBar
+    width: 100%
+    background: #e5dcd0
+    height: 5rem
+    position: relative
+    display: flex
+    box-shadow: 0 2px 10px 0 rgba(166,166,166,1)
+
+  .logoMadera
+    position: absolute
+    top: 0
+    left: 0
+    display: flex
+    justify-content: center
+    align-items: center
+    height: 100%
+
+  .logoMadera img
+    height: 100%
+    margin-top: 0.5rem
+
+  .btnMyAccount
+    position: absolute
+    right: 1rem
+    display: flex
+    justify-content: center
+    align-items: center
+    height: 100%
+</style>
