c2cd00762465977f26b28a7c52d0fa89cf5c5dbe
mickael <mickael.bayer@agreenculture.fr>
2020-02-23 00:05:05 +0100

    [UPD] #15 #16 # 17 :Select client, add client

--- src/views/ListCustomers.vue
+++ src/views/ListCustomers.vue
@@ -1,14 +1,43 @@
-<template lang="pug">
-    .zoneItem
-      .homeItem
-        .item(@click="redirectItem1")
-          img(class="itemImg" src='../assets/add.svg')
-          .itemTitle Nouveau client
-      .homeItem
-        .item(@click="redirectItem2")
-          img(class="itemImg" src='../assets/view.svg')
-          .itemTitle Client existant
-      div {{ customers }}
+<template>
+  <div class="home">
+    <v-layout child-flex>
+      <v-card>
+        <v-card-title>
+          Clients
+          <v-spacer></v-spacer>
+          <v-text-field
+            v-model="search"
+            append-icon="search"
+            label="Rechercher"
+            single-line
+            hide-details
+          ></v-text-field>
+        </v-card-title>
+        <v-data-table
+          :headers="headers"
+          :items="customers"
+          :search="search"
+          hide-default-footer=''
+          item-key="mail"
+        >
+          <template v-slot:body="{ items }">
+            <tbody>
+              <tr :class="item === selectedRow ? 'custom-highlight-row' : ''" @click="handleClick(item)" v-for="item in items" :key="item.mail">
+                <td>{{ item.lastName }}</td>
+                <td>{{ item.firstName }}</td>
+                <td>{{ item.mail }}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-data-table>
+        <v-btn outlined='' class="cancelCreate" right='' color="#d92616" @click='backHome()'> Annuler </v-btn>
+        <v-btn outlined=''  right='' color="#409a1b" @click='validateCustomer()'> Valider </v-btn>
+      </v-card>
+    </v-layout>
+    <v-alert type='error' width="100%" class="alertSelection" icon="error" v-if="isAlertShowned">
+      Pas de client sélectionné.
+    </v-alert>
+  </div>
 </template>
 
 
@@ -21,6 +50,14 @@
     },
     data() {
       return {
+        isAlertShowned: false,
+        search: '',
+        selectedRow: null,
+        headers: [
+          { text: 'Nom', align: 'left', value: 'lastName' },
+          { text: 'Prénom', align: 'left', value: 'firstName' },
+          { text: 'mail', align: 'left', value: 'mail' }
+        ],
         customers: []
       }
     },
@@ -29,14 +66,25 @@
       this.customers = response.data;
     },
     methods: {
-      redirectItem1(){
-        //fomulaire creation client
-        this.$router.push('/addModule')
+      backHome() {
+        this.$router.push('/home')
       },
-      redirectItem2(){
-        //recherche clients existants
-        this.$router.push('/projectList')
-      }
+      handleClick(value){
+        this.selectedRow = value
+      },
+      validateCustomer(){
+        if(!this.selectedRow){
+          this.isAlertShowned = true
+          setTimeout(() => {
+              this.isAlertShowned = false
+          }, 2000)
+          return
+        }
+        else {
+          this.$store.commit('setCustomer', this.selectedRow)
+          this.$router.push('/createProject')
+        }
+      },
     }
   }
 </script>
@@ -48,6 +96,7 @@
     display: flex
     flex-direction: row
     justify-content: center
+    padding: 0.8rem
   .zoneItem
     display: flex
     width: 80%
@@ -77,6 +126,20 @@
   .itemImg
     height: 80%
     margin: 1rem 0
+  .custom-highlight-row
+    background-color: #409a1b
+  .btnCreate
+    margin-top: 3vh
+  .cancelCreate
+    margin-right: 1rem
+  .alertSelection
+    position: absolute
+    top: 4.7rem
+  td
+    vertical-align: left
+    text-align: left
+    cursor: pointer
+
   @media(max-width: 850px)
     .zoneItem
       flex-direction: column
